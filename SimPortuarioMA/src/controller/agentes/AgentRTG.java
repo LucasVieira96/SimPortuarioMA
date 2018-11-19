43b64508f3025e93b6ecb5e7b8600cf7e9df47d6
Lucas <Lucas@LUCAS-PC>
2018-11-18 22:37:53 -0200

    Implementando RTG

--- SimPortuarioMA/src/controller/agentes/AgentRTG.java
+++ SimPortuarioMA/src/controller/agentes/AgentRTG.java
@@ -5,14 +5,28 @@
  */
 package controller.agentes;
 
+import controller.ambiente.Container;
+import controller.ambiente.MapaPilha;
+import controller.ambiente.PilhaContainer;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import jade.util.leap.Iterator;
+import java.util.List;
+import java.util.Stack;
 
 /**
  *
  * @author Lucas
  */
-public class AgentRTG extends Agent{
+public class AgentRTG extends Agent {
+
+    private PilhaContainer pilha;
+    private PilhaContainer pilhaNavio;
+    private List<MapaPilha> cntrSelecionados;
+    private Stack<Container> pilhaTemp;
 
     @Override
     protected void setup() {
@@ -25,5 +39,42 @@ public class AgentRTG extends Agent{
         while (it.hasNext()) {
             System.out.println("- " + it.next());
         }
+        pilhaTemp = new Stack<Container>();
+        pilha = (PilhaContainer) getArguments()[1];
+        pilhaNavio = (PilhaContainer) getArguments()[2];
+        cntrSelecionados = (List<MapaPilha>) getArguments()[3];
+
+        addBehaviour(new CyclicBehaviour(this) {
+            @Override
+            public void action() {
+                MessageTemplate MT1 = MessageTemplate.MatchSender(new AID("Main", AID.ISLOCALNAME));
+                ACLMessage msg = receive(MT1);
+                if (msg != null) {
+                    if (msg.getOntology().equalsIgnoreCase("Atracação Navio")) {
+                        System.out.println(getAID().getLocalName() + ": Iniciando operação!");
+                        for (MapaPilha cntrSelecionado : cntrSelecionados) {
+                            for (int i = 0;
+                                    i < pilha.getPilha()[cntrSelecionado.getPosWidth()]
+                                            [cntrSelecionado.getPosHeight()].size(); i++) {
+                                Container container = pilha.popContainer(
+                                        cntrSelecionado.getPosWidth(),
+                                        cntrSelecionado.getPosHeight());
+                                if (cntrSelecionado.getContainer() == container) {
+                                    pilhaNavio.pushContainer(container, cntrSelecionado.getPosWidth(), cntrSelecionado.getPosHeight());
+                                    for (int j = 0; j < pilhaTemp.size(); j++) {
+                                        pilha.pushContainer(pilhaTemp.pop(),
+                                                cntrSelecionado.getPosWidth(),
+                                                cntrSelecionado.getPosHeight());
+                                    }
+                                    break;
+                                } else {
+                                    pilhaTemp.push(container);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 }
